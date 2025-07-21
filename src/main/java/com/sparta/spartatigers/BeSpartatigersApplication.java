f59e5013758e83018227444544eea5174985b455
jungmin <jungmini0601@gmail.com>
2025-07-21 15:26:05 +0900

    Initial Commit

---
+++ src/main/java/com/sparta/spartatigers/BeSpartatigersApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartatigers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpartatigersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSpartatigersApplication.class, args);
+    }
+
+}
