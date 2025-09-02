2723d7194f790dccd077001ac94edfd1f1cba6a4
harinbeee <luv_lyn@naver.com>
2025-09-02 16:26:14 +0900

    feat : 비정상 종료시 conncection 삭제하는 코드 추가 #14

--- src/main/java/com/sparta/spartatigers/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/spartatigers/domain/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.sparta.spartatigers.domain.user.repository;
 
 import com.sparta.spartatigers.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -10,4 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u.nickname FROM users u WHERE u.id = :userId")
+    Optional<String> findNicknameById(Long userId);
+
 }
---
+++ src/main/java/com/sparta/spartatigers/global/response/WebSocketErrorResponse.java
@@ -0,0 +1,18 @@
+package com.sparta.spartatigers.global.response;
+
+import com.sparta.spartatigers.global.exception.ExceptionCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class WebSocketErrorResponse {
+
+	private final String errorType;
+	private final String message;
+
+	public static WebSocketErrorResponse from(ExceptionCode code) {
+		return WebSocketErrorResponse.of(code.name(), code.getMessage());
+	}
+}
