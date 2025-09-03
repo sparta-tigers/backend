47955a8244652e6598772ed8eecaf00be2827fb2
hyun2y00 <pottq577@naver.com>
2025-09-03 16:20:56 +0900

    feat: Firebase 빈 등록으로 변경
    PostContruct 초기화 대신 Bean으로 등록하여 의존성 주입하여 깔끔히 사용

--- src/main/java/com/sparta/spartatigers/global/config/FirebaseConfig.java
+++ src/main/java/com/sparta/spartatigers/global/config/FirebaseConfig.java
@@ -3,28 +3,43 @@ package com.sparta.spartatigers.global.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
+import com.google.firebase.messaging.FirebaseMessaging;
 import java.io.IOException;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
-    @PostConstruct
-    public void initialize() {
-        try {
-            FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
-                .build();
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        log.info("Initializing Firebase");
 
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-                log.info("FirebaseApp has been initialized successfully.");
+        FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .build();
+
+        List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+        if (firebaseApps != null && !firebaseApps.isEmpty()) {
+            for (FirebaseApp app : firebaseApps) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    log.info("Firebase has already benn initialized");
+                    return app;
+                }
             }
-        } catch (IOException e) {
-            log.error("Error initializing FirebaseApp: {}", e.getMessage());
         }
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+        log.info("FirebaseApp has been initialized successfully.");
+
+        return app;
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
