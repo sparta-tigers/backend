0e6e96aeb5f15df09853f6097d6864e801ecd701
harinbeee <luv_lyn@naver.com>
2025-09-19 13:55:04 +0900

    feat: flyway 사용을 위해 V1(베이스), V2(수정사항) SQL 파일 생성 #25

---
+++ src/main/resources/db/migration/V1_init_schema.sql
@@ -0,0 +1,305 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for macos13.7 (x86_64)
+--
+-- Host: localhost    Database: sparta-tigers
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `sparta-tigers`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sparta-tigers` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `sparta-tigers`;
+
+--
+-- Table structure for table `alarms`
+--
+
+DROP TABLE IF EXISTS `alarms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alarms` (
+  `alarm_id` bigint NOT NULL AUTO_INCREMENT,
+  `normal_alarm_time` datetime(6) DEFAULT NULL,
+  `pre_alarm_time` datetime(6) DEFAULT NULL,
+  `match_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`alarm_id`),
+  KEY `FKmselj0im24os9ewt3ja617neb` (`match_id`),
+  KEY `FKilo4nfh1j7nsepgoq8qf3208a` (`user_id`),
+  CONSTRAINT `FKilo4nfh1j7nsepgoq8qf3208a` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FKmselj0im24os9ewt3ja617neb` FOREIGN KEY (`match_id`) REFERENCES `matches` (`match_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `direct_message`
+--
+
+DROP TABLE IF EXISTS `direct_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `direct_message` (
+  `direct_message_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `message` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sent_at` datetime(6) NOT NULL,
+  `direct_room_id` bigint NOT NULL,
+  `sender_id` bigint NOT NULL,
+  PRIMARY KEY (`direct_message_id`),
+  KEY `FKjruv8jpefkjltrff0ijd9ven2` (`direct_room_id`),
+  KEY `FKnasb3kidlfese478rcfvcx8v7` (`sender_id`),
+  CONSTRAINT `FKjruv8jpefkjltrff0ijd9ven2` FOREIGN KEY (`direct_room_id`) REFERENCES `direct_rooms` (`direct_room_id`),
+  CONSTRAINT `FKnasb3kidlfese478rcfvcx8v7` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `direct_rooms`
+--
+
+DROP TABLE IF EXISTS `direct_rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `direct_rooms` (
+  `direct_room_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `completed_at` datetime(6) DEFAULT NULL,
+  `is_completed` bit(1) NOT NULL,
+  `exchange_request_id` bigint NOT NULL,
+  `receiver_id` bigint NOT NULL,
+  `sender_id` bigint NOT NULL,
+  PRIMARY KEY (`direct_room_id`),
+  KEY `FK9yatw1d8gdw0rccfvp5691u1l` (`exchange_request_id`),
+  KEY `FK7jfy3hn03q0w381y7rauo5vh6` (`receiver_id`),
+  KEY `FK3mcgnhl5gbronfir2p0onx5ca` (`sender_id`),
+  CONSTRAINT `FK3mcgnhl5gbronfir2p0onx5ca` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FK7jfy3hn03q0w381y7rauo5vh6` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FK9yatw1d8gdw0rccfvp5691u1l` FOREIGN KEY (`exchange_request_id`) REFERENCES `exchange_request` (`match_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `exchange_request`
+--
+
+DROP TABLE IF EXISTS `exchange_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exchange_request` (
+  `match_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `status` enum('ACCEPTED','COMPLETED','PENDING','REJECTED') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `item_id` bigint NOT NULL,
+  `receiver_id` bigint NOT NULL,
+  `sender_id` bigint NOT NULL,
+  PRIMARY KEY (`match_id`),
+  KEY `FKecopmihnwlyssp9aas38gv8n2` (`item_id`),
+  KEY `FK5i3lsdy6u2x3hcpxhn2rn771c` (`receiver_id`),
+  KEY `FKi2kdwp05fmpow02a9g3ak9yes` (`sender_id`),
+  CONSTRAINT `FK5i3lsdy6u2x3hcpxhn2rn771c` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FKecopmihnwlyssp9aas38gv8n2` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`),
+  CONSTRAINT `FKi2kdwp05fmpow02a9g3ak9yes` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `favorite_team`
+--
+
+DROP TABLE IF EXISTS `favorite_team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `favorite_team` (
+  `match_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `team_id` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`match_id`),
+  UNIQUE KEY `UKmq89go47i5gg4m138g4mej2o4` (`user_id`),
+  KEY `FK9hgv2bvsm6smbsjaksii8qxn5` (`team_id`),
+  CONSTRAINT `FK7vpxpyednx7rrixvi2x6yg77u` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FK9hgv2bvsm6smbsjaksii8qxn5` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `item_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `category` enum('GOODS','TICKET') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `seat_info` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('COMPLETED','FAILED','REGISTERED') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `version` bigint DEFAULT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`item_id`),
+  UNIQUE KEY `UNIQUE_USER_ITEM` (`user_id`,`created_date`),
+  KEY `idx_item_user_status_created` (`user_id`,`status`,`created_at` DESC),
+  KEY `idx_item_status_created_date` (`status`,`created_date`),
+  CONSTRAINT `FKft8pmhndq1kntvyfaqcybhxvx` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `matches`
+--
+
+DROP TABLE IF EXISTS `matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `matches` (
+  `match_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `away_score` int DEFAULT NULL,
+  `home_score` int DEFAULT NULL,
+  `match_result` enum('AWAY_WIN','CANCEL','DRAW','HOME_WIN','NOT_PLAYED') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `match_time` datetime(6) DEFAULT NULL,
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `reservation_open_time` datetime(6) DEFAULT NULL,
+  `away_team_id` bigint NOT NULL,
+  `home_team_id` bigint NOT NULL,
+  `stadium_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`match_id`),
+  KEY `FK2e8erbfecb0tjtq9iudg36bxu` (`away_team_id`),
+  KEY `FK8k68nekawp47js52dq8720voe` (`home_team_id`),
+  KEY `FKrrg1mqie8f84r20oh3vdb36m8` (`stadium_id`),
+  CONSTRAINT `FK2e8erbfecb0tjtq9iudg36bxu` FOREIGN KEY (`away_team_id`) REFERENCES `teams` (`team_id`),
+  CONSTRAINT `FK8k68nekawp47js52dq8720voe` FOREIGN KEY (`home_team_id`) REFERENCES `teams` (`team_id`),
+  CONSTRAINT `FKrrg1mqie8f84r20oh3vdb36m8` FOREIGN KEY (`stadium_id`) REFERENCES `stadiums` (`stadium_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oauths`
+--
+
+DROP TABLE IF EXISTS `oauths`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `oauths` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `provider` enum('GOOGLE','KAKAO') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `provider_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKoqly8u8vdhlt41bvxgtni3nlq` (`user_id`),
+  CONSTRAINT `FKoqly8u8vdhlt41bvxgtni3nlq` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `player_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `position` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `team_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`player_id`),
+  KEY `FK5nglidr00c4dyybl171v6kask` (`team_id`),
+  CONSTRAINT `FK5nglidr00c4dyybl171v6kask` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stadiums`
+--
+
+DROP TABLE IF EXISTS `stadiums`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stadiums` (
+  `stadium_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`stadium_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams` (
+  `team_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `code` enum('HH','HT','KT','LG','LT','NC','OB','SK','SS','WO') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `path` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`team_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nickname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profile_image_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` enum('ROLE_ADMIN','ROLE_USER') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-19 13:17:20
