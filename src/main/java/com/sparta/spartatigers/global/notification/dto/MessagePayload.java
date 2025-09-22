e7eb1bba48ed7ef34f3138b9c4c08a94f604dee8
hyun2y00 <pottq577@naver.com>
2025-09-22 13:21:50 +0900

    feat: 메세지 dto 필드 확장

--- src/main/java/com/sparta/spartatigers/global/notification/dto/MessagePayload.java
+++ src/main/java/com/sparta/spartatigers/global/notification/dto/MessagePayload.java
@@ -1,5 +1,6 @@
 package com.sparta.spartatigers.global.notification.dto;
 
+import java.util.Map;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MessagePayload {
 
-    private final String subject;
-    private final String message;
+    private final AlertLevel level;                 // 심각도
+    private final String subject;                   // 알림 제목
+    private final String message;                   // 상세 메세지
+    private final Map<String, String> metadata;     // 추가 정보
+    private final String backlink;                  // 관련 링크 (그라파나 대시보드 등)
 }
