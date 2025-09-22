1386312f41e3242c7a9191b235e6fe4f3f548b97
hyun2y00 <pottq577@naver.com>
2025-09-22 13:39:24 +0900

    feat: 디스코드 알람을 구조화된 dto 형식으로 전송 및 메서드 리팩토링

--- src/main/java/com/sparta/spartatigers/global/notification/DiscordNotificationSender.java
+++ src/main/java/com/sparta/spartatigers/global/notification/DiscordNotificationSender.java
@@ -1,8 +1,11 @@
 package com.sparta.spartatigers.global.notification;
 
+import com.sparta.spartatigers.global.notification.dto.AlertLevel;
+import com.sparta.spartatigers.global.notification.dto.DiscordWebhookPayload;
+import com.sparta.spartatigers.global.notification.dto.DiscordWebhookPayload.Embed;
 import com.sparta.spartatigers.global.notification.dto.MessagePayload;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,17 +28,42 @@ public class DiscordNotificationSender implements NotificationSender {
             return;
         }
 
-        Map<String, String> message = new HashMap<>();
-        message.put("content", "## " + payload.getSubject() + "\n" + "```\n" + payload.getMessage() + "\n```");
+        DiscordWebhookPayload discordPayload = createDiscordMessage(payload);
 
         try {
             restClient.post()
                 .uri(discordWebhookUrl)
-                .body(message)
+                .body(discordPayload)
                 .retrieve()
                 .toBodilessEntity();
         } catch (Exception e) {
             log.error("디스코드 알림 전송에 실패했습니다.", e);
         }
+    }
+
+    private DiscordWebhookPayload createDiscordMessage(MessagePayload payload) {
+        Embed embed = Embed.builder()
+            .title(payload.getSubject())
+            .description(payload.getMessage())
+            .color(getColorForLevel(payload.getLevel()))
+            .url(payload.getBacklink())
+            .fields(
+                (payload.getMetadata() != null) ?
+                    payload.getMetadata().entrySet().stream()
+                        .map(entry -> new DiscordWebhookPayload.Embed.Field(entry.getKey(), entry.getValue(), true))
+                        .collect(Collectors.toList()) : null
+            )
+            .build();
+
+        return new DiscordWebhookPayload(List.of(embed));
+    }
+
+    private int getColorForLevel(AlertLevel level) {
+        return switch (level) {
+            case INFO -> 3447003;       // Blue
+            case WARN -> 15105570;      // Yellow
+            case ERROR -> 15158332;     // Red
+            case CRITICAL -> 10038562;  // Dark Red
+        };
     }
 }
