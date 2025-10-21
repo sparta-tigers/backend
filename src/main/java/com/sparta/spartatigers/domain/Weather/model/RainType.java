f54fcbe1a246eb8f891df6c6be53c4b088369eaf
harinbeee <luv_lyn@naver.com>
2025-10-21 17:00:59 +0900

    feat : 원본 응답의 코드값 enum으로 정의 #31

--- src/main/java/com/sparta/spartatigers/domain/Weather/model/RainType.java
+++ src/main/java/com/sparta/spartatigers/domain/Weather/model/RainType.java
@@ -1,4 +1,34 @@
-package com.sparta.spartatigers.domain.Weather.model;
+package com.sparta.spartatigers.domain.weather.model;
 
 public enum RainType {
+	NONE(0, "없음"),
+	RAIN(1, "비"),
+	RAIN_SNOW(2, "비/눈"),
+	SNOW(3, "눈"),
+	RAINDROP(5, "빗방울"),
+	RAINDROP_SNOW_FLYING(6, "빗방울눈날림"),
+	SNOW_FLYING(7, "눈날림");
+
+	private final int code;
+	private final String description;
+
+	RainType(int code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	public static RainType fromCode (String code) {
+		if(code == null) return NONE;
+		return switch (code) {
+			case "0" -> NONE;
+			case "1" -> RAIN;
+			case "2" -> RAIN_SNOW;
+			case "3" -> SNOW;
+			case "5" -> RAINDROP;
+			case "6" -> RAINDROP_SNOW_FLYING;
+			case "7" -> SNOW_FLYING;
+			default -> NONE;
+		};
+	}
+
 }
