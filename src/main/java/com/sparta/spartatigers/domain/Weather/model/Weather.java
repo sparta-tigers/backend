c4e91e349567226efd7a50f4b028fb2ba31e9c10
harinbeee <luv_lyn@naver.com>
2025-10-21 17:04:25 +0900

    feat : 기상청 api 가이드와 필드명 일치하도록 수정 #31

--- src/main/java/com/sparta/spartatigers/domain/Weather/model/Weather.java
+++ src/main/java/com/sparta/spartatigers/domain/Weather/model/Weather.java
@@ -1,10 +1,12 @@
-package com.sparta.spartatigers.domain.Weather.model;
+package com.sparta.spartatigers.domain.weather.model;
 
 public class Weather {
-	private double temperature; //기온(TMP) - 초단기실황, 초단기예보
-	private SkyStatus skyStatus; // 하늘상태(SKY) - 초단기예보, 단기예보
-	private int rainProbability; // 강수확률(POP) - 단기예보
-	private RainType rainType; // 강수형태(PYT) - 초단기예보
-	private double rainAmount; // 강수량(RN1/PCP) - 초단기예보
+	private double temperature; //기온(TMP)
+	private SkyStatus skyStatus;// 하늘상태(SKY)
+	private double windSpeed; // 풍속(WSD)
+	private WindDirection windDirection; // 풍향(VEC)
+	private double rainAmount; //강수량(RN1/PCP)
+	private int rainProbability; // 강수확률(POP)
+	private RainType rainType; // 강수형태(PYT)
 
 }
