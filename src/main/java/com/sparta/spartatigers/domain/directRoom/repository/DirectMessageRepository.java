dd4d63b6d599a33a2964cd7f32f8790f4f53f6d2
harinbeee <luv_lyn@naver.com>
2025-11-07 16:52:07 +0900

    fix : 쿼리 오타 수정, @Param 추가 #35

--- src/main/java/com/sparta/spartatigers/domain/directRoom/repository/DirectMessageRepository.java
+++ src/main/java/com/sparta/spartatigers/domain/directRoom/repository/DirectMessageRepository.java
@@ -1,5 +1,7 @@
 package com.sparta.spartatigers.domain.directRoom.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +17,22 @@ public interface DirectMessageRepository extends JpaRepository<DirectMessage, Lo
             @Param("roomId") Long roomId, Pageable pageable);
 
     void deleteAllByDirectRoomId(Long directRoomId);
+
+    @Query("""
+        SELECT COUNT(m)
+        FROM direct_message m
+        WHERE m.directRoom.id = :roomId
+            AND m.sender.id <> :userId
+            AND m.isRead = false
+        """)
+    long countUnreadMsg(@Param("roomId") Long roomId, @Param("userId")Long userId);
+
+    @Query("""
+        SELECT m
+        FROM direct_message m
+        WHERE m.directRoom.id = :roomId
+            AND m.sender.id <> :userId
+            AND m.isRead = false
+        """)
+    List<DirectMessage> findUnreadMsg(@Param("roomId")Long roomId, @Param("userId")Long userId);
 }
