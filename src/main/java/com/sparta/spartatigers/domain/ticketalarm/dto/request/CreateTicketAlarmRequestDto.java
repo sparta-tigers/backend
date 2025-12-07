35d617adb320fca96094e03f943123c06862b47c
harinbeee <luv_lyn@naver.com>
2025-12-07 16:21:00 +0900

    feat : 예매 알람 요청/응답 dto 생성 # 40

--- src/main/java/com/sparta/spartatigers/domain/ticketalarm/dto/request/CreateTicketAlarmRequestDto.java
+++ src/main/java/com/sparta/spartatigers/domain/ticketalarm/dto/request/CreateTicketAlarmRequestDto.java
@@ -1,11 +1,15 @@
 package com.sparta.spartatigers.domain.ticketalarm.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class CreateTicketAlarmRequestDto {
 
 	Long matchId;
-	Long TeamId;
+	Long teamId;
 	Integer preAlarmTime;
-
+	String membership;
 
 }
