2da3dbe80b9803dc88096d3838e732cf2ab6545f
harinbeee <luv_lyn@naver.com>
2025-12-09 15:56:43 +0900

    feat : 예매알림에서 사용할 새 익셉션 추가 # 40

--- src/main/java/com/sparta/spartatigers/global/exception/enums/ExceptionCode.java
+++ src/main/java/com/sparta/spartatigers/global/exception/enums/ExceptionCode.java
@@ -47,6 +47,8 @@ public enum ExceptionCode {
     ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E404, "알람을 찾을 수 없습니다.", LogLevel.DEBUG),
     ALARM_TIME_BEFORE_NOW(HttpStatus.BAD_REQUEST, ErrorCode.E400, "알람 시간이 현재보다 이전입니다.", LogLevel.DEBUG),
     PRE_ALARM_TIME_BEFORE_NOW(HttpStatus.BAD_REQUEST, ErrorCode.E400, "알람 시간이 현재보다 이전입니다.", LogLevel.DEBUG),
+    BOOKING_SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E404, "예매 오픈 일정을 계산할 수 없습니다.", LogLevel.DEBUG),
+    POLICY_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E404, "멤버쉽을 찾을 수 없습니다.", LogLevel.DEBUG),
 
     // 채팅방
     CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E404, "채팅방이 존재하지 않습니다.", LogLevel.DEBUG),
