ffd8e12c96cb9c2d625c08ed93da8de31824c69e
harinbeee <luv_lyn@naver.com>
2026-02-03 16:49:44 +0900

    refactor : 베이스 엔티티 내 Prepersist, PreUpdate 추가 # 44

--- src/main/java/com/sparta/spartatigers/domain/common/entity/BaseEntity.java
+++ src/main/java/com/sparta/spartatigers/domain/common/entity/BaseEntity.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,10 +25,21 @@ import lombok.experimental.SuperBuilder;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
-    @Column(nullable = false)
-    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
