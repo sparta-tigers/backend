228e7026293d8e23902ddb05146ec460e91f66e6
harinbeee <luv_lyn@naver.com>
2026-03-19 14:40:14 +0900

    feat : LocalImageStorageService 사용해 이미지 업로드 기능 추가, 티켓 이미지 OCR 사용 후 삭제 여부 비즈니스 규칙 논의 필요 ( 현재는 업로드함 ) # 46

--- src/main/java/com/sparta/spartatigers/domain/matchAttendance/controller/MatchAttendanceController.java
+++ src/main/java/com/sparta/spartatigers/domain/matchAttendance/controller/MatchAttendanceController.java
@@ -1,9 +1,10 @@
 package com.sparta.spartatigers.domain.matchAttendance.controller;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -46,15 +47,17 @@ public class MatchAttendanceController {
 	public ApiResponse<TicketOcrResponseDto> uploadTicketAndOcr(
 		@RequestPart(value = "images", required = true) MultipartFile ticket
 	) {
+		log.info("티켓 좌석 추출 시작 ==");
 		if (ticket == null || ticket.isEmpty()) {
 			throw new InvalidRequestException(ExceptionCode.INVALID_TICKET_IMAGE);
 		}
 		try {
+			String fullText = ocrService.extractTextFromImage(ticket);
+			String parsedSeat = ocrService.parseSeatInfo(fullText);
+
 			List<String> imageUrls = imageStorageService.uploadImages(List.of(ticket));
 			String uploadedImageUrl = imageUrls.isEmpty() ? null : imageUrls.get(0);
 
-			String fullText = ocrService.extractTextFromImage(ticket);
-			String parsedSeat = ocrService.parseSeatInfo(fullText);
 			TicketOcrResponseDto dto = TicketOcrResponseDto.from(uploadedImageUrl, parsedSeat);
 
 			return ApiResponse.success(dto);
@@ -62,23 +65,40 @@ public class MatchAttendanceController {
 			log.error("직관기록 - 티켓 OCR 중 오류 발생 / 파일명 ={}", ticket.getOriginalFilename(), e);
 			throw new InvalidRequestException(ExceptionCode.TICKET_OCR_ERROR);
 		}
-
 	}
 
-	@PostMapping
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ApiResponse<MatchAttendanceResponseDto> createAttendance(
-		@Valid @RequestParam MatchAttendanceRequestDto request,
-		@Auth TokenClaim tokenClaim
+		@Auth TokenClaim tokenClaim,
+		@RequestPart(value = "request", required = false) MatchAttendanceRequestDto request,
+		@RequestPart(value = "images", required = false) List<MultipartFile> images
 	) {
 		Long userId = tokenClaim.getUserId();
-		return ApiResponse.created(matchAttendanceService.createAttendance(userId, request));
+
+		List<String> uploadedImageUrls = new ArrayList<>();
+		if(images != null && !images.isEmpty()) {
+			uploadedImageUrls = imageStorageService.uploadImages(images);
+		}
+
+		try{
+			MatchAttendanceResponseDto response = matchAttendanceService.createAttendance(userId, request, uploadedImageUrls);
+			return ApiResponse.created(response);
+		} catch (Exception e) {
+			if(!uploadedImageUrls.isEmpty()) {
+				log.error("직관기록 이미지 저장 실패, 업로드 된 파일 롤백 : {}", uploadedImageUrls);
+				imageStorageService.deleteImages(uploadedImageUrls);
+			}
+			throw e;
+		}
 	}
 
 	@GetMapping("/{attendanceId}")
 	public ApiResponse<MatchAttendanceResponseDto> getAttendance(
+		@Auth TokenClaim tokenClaim,
 		@PathVariable Long attendanceId
 	) {
-		return ApiResponse.success(matchAttendanceService.getAttendance(attendanceId));
+		Long userId = tokenClaim.getUserId();
+		return ApiResponse.success(matchAttendanceService.getAttendance(userId, attendanceId));
 	}
 
 	@GetMapping("/my")
@@ -91,15 +111,30 @@ public class MatchAttendanceController {
 		return ApiResponse.success(matchAttendanceService.getAllAttendance(userId, page, size));
 	}
 
-	@PatchMapping("/{attendanceId}")
+	@PatchMapping(value = "/{attendanceId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ApiResponse<MatchAttendanceResponseDto> updateAttendance (
 		@Auth TokenClaim tokenClaim,
 		@PathVariable Long attendanceId,
-		@RequestParam MatchAttendanceUpdateRequestDto request
+		@RequestPart (value = "request") MatchAttendanceUpdateRequestDto request,
+		@RequestPart(value = "images", required = false) List<MultipartFile> newImages
 	) {
 		Long userId = tokenClaim.getUserId();
-		return ApiResponse.success(matchAttendanceService.updateAttendance(userId, attendanceId, request));
 
+		List<String> uploadedImageUrls = new ArrayList<>();
+		if(newImages != null && !newImages.isEmpty()) {
+			uploadedImageUrls = imageStorageService.uploadImages(newImages);
+		}
+
+		try{
+			MatchAttendanceResponseDto response = matchAttendanceService.updateAttendance(userId, attendanceId, request, uploadedImageUrls);
+			return ApiResponse.success(response);
+		} catch (Exception e) {
+			if(!uploadedImageUrls.isEmpty()) {
+				log.error("직관기록 이미지 수정 실패, 업로드 된 파일 롤백 : {}", uploadedImageUrls);
+				imageStorageService.deleteImages(uploadedImageUrls);
+			}
+			throw e;
+		}
 	}
 
 	@DeleteMapping("/{attendanceId}")
