3d40e13395f08eff7e04bf4036e813d8806c042a
hyun2y00 <pottq577@naver.com>
2026-04-25 13:42:28 +0900

    [CHORE] 함수형 인덱스 사용 시 MySQL 버전 상세히 명시

--- src/main/resources/db/migration/V22__drop_and_create_conditional_unique_item.sql
+++ src/main/resources/db/migration/V22__drop_and_create_conditional_unique_item.sql
@@ -2,6 +2,6 @@
 ALTER TABLE items DROP INDEX UNIQUE_USER_ITEM;
 
 -- 2. 'REGISTERED' 상태인 경우에만 적용되는 조건부 유니크 인덱스 생성
--- MySQL 8.0+ Functional Index 활성화
+-- MySQL 8.0.13+ Functional Index 활성화
 -- 상태가 'REGISTERED' 아닐 경우(DELETED 등) NULL을 반환하여 Unique 제약에서 제외됩니다.
 ALTER TABLE items ADD UNIQUE INDEX UK_ACTIVE_ITEM_PER_USER (user_id, (IF(status = 'REGISTERED', status, NULL)));
