907dd1c9e66a8d5881706a58ce2df162bbca14ff
hyun2y00 <pottq577@naver.com>
2026-05-08 18:58:18 +0900

    [REFACTOR] Phase 4: 코드 품질 및 롬복 정리
    - HomeAway: 명시적 Getter(getCode)가 존재하므로 클래스 레벨의 중복 @Getter 어노테이션 제거

--- src/main/java/com/sparta/spartatigers/domain/match/model/HomeAway.java
+++ src/main/java/com/sparta/spartatigers/domain/match/model/HomeAway.java
@@ -2,13 +2,11 @@ package com.sparta.spartatigers.domain.match.model;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  * 경기 장소 타입 (홈/어웨이)
  */
-@Getter
 @RequiredArgsConstructor
 public enum HomeAway {
     HOME("H"),
