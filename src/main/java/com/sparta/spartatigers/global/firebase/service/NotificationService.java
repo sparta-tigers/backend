32c38bf9a3b0ac32a16845ceba1bd0968b23fa47
anyubinn <anyubin0724@naver.com>
2026-05-27 18:02:16 +0900

    refactor: 알림과 교환 사이의 트랜잭션 처리 분리

--- src/main/java/com/sparta/spartatigers/global/firebase/service/NotificationService.java
+++ src/main/java/com/sparta/spartatigers/global/firebase/service/NotificationService.java
@@ -2,8 +2,12 @@ package com.sparta.spartatigers.global.firebase.service;
 
 import com.sparta.spartatigers.global.firebase.dto.NotificationMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
@@ -17,5 +21,26 @@ public class NotificationService {
         }
 
         fcmService.sendMessageToToken(message.token(), message.title(), message.body());
+    }
+
+    public void sendAfterCommit(NotificationMessage message) {
+        if (TransactionSynchronizationManager.isSynchronizationActive()) {
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                @Override
+                public void afterCommit() {
+                    sendSafely(message);
+                }
+            });
+        } else {
+            sendSafely(message);
+        }
+    }
+
+    private void sendSafely(NotificationMessage message) {
+        try {
+            send(message);
+        } catch (Exception e) {
+            log.warn("[FCM 알림 발송 실패] message={}", message, e);
+        }
     }
 }
