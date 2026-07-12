3301d49034616fb1fa6c8c2a80cc7fdd55104644
hyun2y00 <pottq577@naver.com>
2026-07-12 16:03:06 +0900

    test: CQRS 패키지 구조에 맞게 테스트 파일 위치 이동
    CQRS 아키텍처(foundation 레이어) 원본 코드의 패키지에 맞추어,
    잘못된 위치에 생성되어 있던 BcryptPasswordEncoderTest와 MatchTest를
    각각 알맞은 foundation 패키지 하위 경로로 이동했습니다.

--- src/test/java/com/sparta/spartatigers/domain/foundation/user/auth/service/BcryptPasswordEncoderTest.java
+++ src/test/java/com/sparta/spartatigers/domain/foundation/user/auth/service/BcryptPasswordEncoderTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 class BcryptPasswordEncoderTest {
 
     BcryptPasswordEncoder encoder = new BcryptPasswordEncoder();
